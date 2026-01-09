3b1712193145d1de5e583dfb63384a5c43ba5f38
JeetPatel7 <jeet.d.kyada@gmail.com>
2026-01-09 15:35:26 +0530

    added comment

--- src/app/api/leaves/[id]/route.ts
+++ src/app/api/leaves/[id]/route.ts
@@ -5,6 +5,7 @@ import { getAuthUser } from "@/lib/auth";
 import { NextResponse } from "next/server";
 import { leaveStatusSchema } from "@/lib/validations";
 
+//
 export async function PATCH(req: Request, { params }: { params: Promise<{ id: string }> }) {
     try {
         await connectDB();
